9636fc974737bbfe0730213201e967222f478cbf
Akashdeep Patra <adeep8961@gmail.com>
2026-03-06 20:12:24 +0530

    fix: add html and body tags to root layout to fix dynamic OG generation

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return children as any;
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
 }
